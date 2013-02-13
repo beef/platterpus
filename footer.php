155536e3c321cef8ec863f0149434cd4a0097f57
discoliam <discoliam@googlemail.com>
2013-02-13 15:21:53 +0000

    tidied up the footer

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
-	<!-- jQuery CDN Failsafe to CloudFlare CDN -->
-	<script>window.jQuery || document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"><\/script>');</script>
+      <!--[if ! lte IE 6]><!-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="javascripts/application.js"></script>
+      <!--<![endif]-->
+    <!-- eo:JavaScripts -->
+	
 	<?php wp_footer(); ?>
 </body>
 </html>
