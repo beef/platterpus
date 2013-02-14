2e9ebd0cfa9be64aefd374d4b018673c35197f73
discoliam <discoliam@googlemail.com>
2013-02-14 12:24:52 +0000

    added some more cleanup tools

--- functions/cleanup.php
+++ functions/cleanup.php
@@ -60,4 +60,16 @@ add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove
 
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
 
+// Originally from http://wpengineer.com/1438/wordpress-header/
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+add_filter('the_generator', '__return_false'); //Remove the WordPress version from RSS feeds
+
+
 ?>
