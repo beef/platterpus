cb0a299c0cd190d23320874a77a5b2de4b6de5a7
discoliam <discoliam@googlemail.com>
2013-02-14 11:42:41 +0000

    re-structured the functions area

--- functions.php
+++ functions.php
@@ -5,11 +5,15 @@
  *  Custom functions, support, custom post types and more.
  */
 
-require_once locate_template('/functions/suppport.php');             // Utility functions
-require_once locate_template('/functions/functions.php');            // Utility functions
-require_once locate_template('/functions/actions.php');              // Utility functions
-require_once locate_template('/functions/filters.php');              // Utility functions
-require_once locate_template('/functions/custom-post-types.php');    // Utility functions
-require_once locate_template('/functions/shortcodes.php');           // Utility functions
+
+require_once locate_template('/functions/support.php');            // Setting up wordpress features
+
+require_once locate_template('/functions/setup.php');								// Default Setup for Wordpress
+require_once locate_template('/functions/cleanup.php');							// Cleanup some Wordpress Mess
+require_once locate_template('/functions/custom.php');							// Custom Features 
+
+
+require_once locate_template('/functions/widget-areas.php');				// Widget Areas
+require_once locate_template('/functions/custom-post-types.php');   // Custom Post Types
 
 ?>
