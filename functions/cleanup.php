f5320d148fa3d104c678494046ebe04c0d3b2d85
Craig Coles <craigc@wearebeef.co.uk>
2013-02-14 14:07:49 +0000

    Removing some features that wont be needed

--- functions/cleanup.php
+++ functions/cleanup.php
@@ -48,6 +48,24 @@ function remove_thumbnail_dimensions( $html ) {
     return $html;
 }
 
+function example_remove_dashboard_widgets() {
+    // Globalize the metaboxes array, this holds all the widgets for wp-admin
+    global $wp_meta_boxes;
+
+    // Remove the incomming links widget
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);   
+
+    // Remove right now
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+}
+
+function platterpus_remove_menu_elements()
+{
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
 
 // Initiate
 //---------------------------------------------------------
@@ -57,8 +75,12 @@ add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline 
 add_filter('style_loader_tag', 'html5_style_remove'); // Remove 'text/css' from enqueued stylesheet
 add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to thumbnails
 add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove width and height dynamic attributes to post images
+add_filter('screen_options_show_screen', '__return_false'); // Hides the screen options tab
 
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
+
+add_action('admin_init', 'platterpus_remove_menu_elements', 102); // Remove menu items
+add_action('wp_dashboard_setup', 'example_remove_dashboard_widgets' ); // Remove dashboard widgets - Incoming links
 
 // Originally from http://wpengineer.com/1438/wordpress-header/
 remove_action('wp_head', 'feed_links', 2);
--- functions/support.php
+++ functions/support.php
@@ -7,8 +7,6 @@
  */
 
 if (function_exists('add_theme_support')) {
-    // Add Menu Support
-    add_theme_support('menus');
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
